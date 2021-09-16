18c628511e8fc355c34769201c020899fa87fb58
tiberius-ls <ogbonnaemeka543@gmail.com>
2021-09-16 15:43:43 +0100

    updated

--- app/javascript/controllers/show_progress_controller.js
+++ app/javascript/controllers/show_progress_controller.js
@@ -6,22 +6,28 @@ export default class extends Controller {
     }
   connect() {
       this.element.addEventListener("direct-upload:progress", this.updateProgress.bind(this))
-    this.element.addEventListener("direct-upload:error", event => {
-          event.preventDefault()
-                const { id, error } = event.detail
-                      console.log(error)
-                          })
-                            }
+
+      this.element.addEventListener("direct-upload:error", event => {
+        event.preventDefault()
+            const { id, error } = event.detail
+            console.log(error)
+        })
+    }
+
   showProgress() {
       this.progressTarget.style.display = "block"
-        }
+    }
+
+
   updateProgress() {
-      const { id, progress } = event.detail
-          this.progressWidthTarget.style.width = `${Math.round(progress)}%`
-              this.progressTextTarget.innerHTML = `${Math.round(progress)}% complete`
-                }
+        const { id, progress } = event.detail
+        this.progressWidthTarget.style.width = `${Math.round(progress)}%`
+        this.progressTextTarget.innerHTML = `${Math.round(progress)}% complete`
+    }
+
+
   disconnect() {
-      this.element.removeEventListener("direct-upload:progress", this.updateProgress)
-        }
-        }
+        this.element.removeEventListener("direct-upload:progress", this.updateProgress)
+    }
+}
           
